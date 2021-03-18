6621a03dafc1bfa828643e8d6f0d61738e0bd313
meghanctucker <meghanctucker@gmail.com>
2021-03-17 21:33:02 -0400

    Cleanup
    Removes unessary images and comments, made JS clearer

--- JS/app.js
+++ JS/app.js
@@ -1,112 +1,119 @@
-//Variables
+
+//Variables - portfolio filter
 const cssButton = document.getElementById('css');
 const javascriptButton = document.getElementById('javascript');
 const apiButton = document.getElementById('api');
 const allButton = document.getElementById('all');
 const portPics = document.getElementsByClassName('mypic');
 const portOverlay = document.getElementsByClassName('overlay');
-  //buttons
-  //Images
-  //caption/attribute
 
+//Variables - Contact Validation
+  //Name Field
+  const nameField = document.getElementById('name');
+  const popNameField = document.getElementById('popName');
+  //email Field
+  const emailField = document.getElementById('email');
+  const popEmail = document.getElementById('popEmail');
+  //message Field
+  const messageField = document.getElementById('message');
+  const popMessageField = document.getElementById('popMessage');
+  //submit button
+  const submit = document.getElementById('submit');
+  const popSubmit = document.getElementById('popSubmit');
+
+  //Variables - contact button modal
+  const contactModal = document.getElementById('contactButton');
+  const modalHTML = document.getElementById('contactForm');
+  const overlay = document.getElementById('overlayInfo');
+  const modalContainer = document.getElementById('modalContent');
+  const modalClose = document.getElementById('modalClose');
+
+//Portfolio Filter
+
+//user clicks CSS filter button
 cssButton.addEventListener('click', () => {
+  //loops through portfolio items
   for(let i = 0; i < portPics.length; i ++) {
     let portItem = portPics[i];
     let overlaySelect = portOverlay[i];
+    //removes any previously hidden items
     portItem.classList.remove('hidden');
     overlaySelect.classList.remove('hidden');
+    //if item label not "css", remove from list
     if (portItem.dataset.filterType !== "css"){
       portItem.classList.add('hidden');
       overlaySelect.classList.add('hidden');
     }
   }
 });
+
+//user clicks JSfilter button
 javascriptButton.addEventListener('click', () => {
+  //loops through portfolio items
   for(let i = 0; i < portPics.length; i ++) {
     let portItem = portPics[i];
     let overlaySelect = portOverlay[i];
+    //removes hidden items
     portItem.classList.remove('hidden');
     overlaySelect.classList.remove('hidden');
+    //hides non JS items
     if (portItem.dataset.filterType !== "javascript"){
       portItem.classList.add('hidden');
       overlaySelect.classList.add('hidden');
     }
   }
 });
+
+//API filter button
 apiButton.addEventListener('click', () => {
+  //loops through portfolio items
   for(let i = 0; i < portPics.length; i ++) {
     let portItem = portPics[i];
     let overlaySelect = portOverlay[i];
+    //removes hidden items
     portItem.classList.remove('hidden');
     overlaySelect.classList.remove('hidden');
+    //if not api, hide
     if (portItem.dataset.filterType !== "api"){
       portItem.classList.add('hidden');
       overlaySelect.classList.add('hidden');
     }
   }
 });
+//Show all button
 allButton.addEventListener('click', () => {
+  //loops through portfolio items
   for(let i = 0; i < portPics.length; i ++) {
     let portItem = portPics[i];
     let overlaySelect = portOverlay[i];
+    //removes hidden items
       portItem.classList.remove('hidden');
       overlaySelect.classList.remove('hidden');
   }
 });
-//portfolio filter
 
-//for(let i = 0; i < portPics.length; i ++) {
-//  let myPortPic = portPics[i];
-//  console.log(myPortPic.attr("data-filter-type"));
-//}
-
-  //when the button is pressed
-  //loop through available image data
-  //add class of "hidden"
-
-  //if "show all" button is clicked
-  //remove all classes of hidden
 
   //**********Contact Button Modal ******
 
-    //Variables
-    const contactModal = document.getElementById('contactButton');
-    const modalHTML = document.getElementById('contactForm');
-    const overlay = document.getElementById('overlayInfo');
-    const modalContainer = document.getElementById('modalContent');
-    const modalClose = document.getElementById('modalClose');
 
     //display modal callback function
     function displayModal() {
       overlay.classList.remove("hidden");
     }
 
+    //shows modal on button press
     contactModal.addEventListener('click', () =>{
       displayModal();
       });
-
+    //hides modal
     modalClose.addEventListener('click', ()=>{
       overlay.classList.add('hidden');
     });
       // **********Contact Validation*******
 
-        //Variables
-
-          //Name Field
-          const nameField = document.getElementById('name');
-          const popNameField = document.getElementById('popName');
-          //email Field
-          const emailField = document.getElementById('email');
-          const popEmail = document.getElementById('popEmail');
-          //message Field
-          const messageField = document.getElementById('message');
-          const popMessageField = document.getElementById('popMessage');
-          //submit button
-          const submit = document.getElementById('submit');
-          const popSubmit = document.getElementById('popSubmit');
 
         //Instructions:
-          //callback function to determine if a field has been filled out
+    //callback function to determine if a field has been filled out
     function sayValue(field){
       if (field.value === "") {
         return false;
